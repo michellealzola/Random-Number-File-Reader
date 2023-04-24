b549a591224be7b82786b5d91c522e7ce76fe3b4
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2023-04-24 16:17:03 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Random_Number_File_Reader
+{
+    partial class frmRandNumFileReader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.lboxRandomNumbersRead = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblRandCount = new System.Windows.Forms.Label();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenFile.Location = new System.Drawing.Point(39, 32);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(555, 51);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Open a File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // lboxRandomNumbersRead
+            // 
+            this.lboxRandomNumbersRead.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lboxRandomNumbersRead.FormattingEnabled = true;
+            this.lboxRandomNumbersRead.ItemHeight = 19;
+            this.lboxRandomNumbersRead.Location = new System.Drawing.Point(39, 89);
+            this.lboxRandomNumbersRead.Name = "lboxRandomNumbersRead";
+            this.lboxRandomNumbersRead.Size = new System.Drawing.Size(555, 194);
+            this.lboxRandomNumbersRead.TabIndex = 1;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(39, 390);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(420, 48);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(487, 390);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(107, 48);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Total of All Random Numbers:";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTotal.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(317, 301);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(277, 35);
+            this.lblTotal.TabIndex = 5;
+            this.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(44, 357);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(234, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Number of Random Numbers:";
+            // 
+            // lblRandCount
+            // 
+            this.lblRandCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblRandCount.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRandCount.Location = new System.Drawing.Point(317, 340);
+            this.lblRandCount.Name = "lblRandCount";
+            this.lblRandCount.Size = new System.Drawing.Size(277, 36);
+            this.lblRandCount.TabIndex = 7;
+            this.lblRandCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFileDialog1";
+            // 
+            // frmRandNumFileReader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 450);
+            this.Controls.Add(this.lblRandCount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lboxRandomNumbersRead);
+            this.Controls.Add(this.btnOpenFile);
+            this.Name = "frmRandNumFileReader";
+            this.Text = "Random Number File Reader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.ListBox lboxRandomNumbersRead;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblRandCount;
+        private System.Windows.Forms.OpenFileDialog openFile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Random_Number_File_Reader
+{
+    public partial class frmRandNumFileReader : Form
+    {
+        public frmRandNumFileReader()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            int num;
+            int total = 0;
+            int count = 0;
+
+            try
+            {
+                if(openFile.ShowDialog() == DialogResult.OK)
+                {
+                    StreamReader inputFile;
+                    inputFile = File.OpenText(openFile.FileName);
+                    
+                    while(!inputFile.EndOfStream)
+                    {
+                        if(int.TryParse(inputFile.ReadLine(), out num))
+                        {
+                            lboxRandomNumbersRead.Items.Add(num.ToString());
+                            total += num;
+                            count++;
+                        }
+                    }
+
+                    lblTotal.Text = total.ToString();
+                    lblRandCount.Text = count.ToString();
+
+                    inputFile.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lboxRandomNumbersRead.Items.Clear();
+            lblTotal.Text = "";
+            lblRandCount.Text = "";
+
+            btnOpenFile.Focus();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
